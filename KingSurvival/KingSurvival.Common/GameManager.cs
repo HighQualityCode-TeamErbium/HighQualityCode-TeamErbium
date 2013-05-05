2fdade98fc4a709f5f1c28a940e3e23623291cbd
yoandinkov <yoandinkov@gmail.com>
2013-05-05 12:44:10 +0300

    Added additinal line for checking if king is winner

--- KingSurvival/KingSurvival.Common/GameManager.cs
+++ KingSurvival/KingSurvival.Common/GameManager.cs
@@ -4,136 +4,166 @@ namespace KingSurvival.Common
 {
     internal static class GameManager
     {
-        
-
-        public static bool HasGameEnded(int turn, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD, Pawn pawnKing)
+        public static bool HasGameEnded(int gameTurn, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD, Pawn pawnKing)
         {
-            bool isKingOnTurn = false;
-            bool isKingWinner = false;
-            // TODO : Refactor this code (yoan)
-            #region /*Change turn input*/
-            if (turn % 2 == 1)
+            bool isKingOnTurn = false;            
+            // on every odd game t king is on turn
+            isKingOnTurn = (gameTurn % 2 == 1);
+            // if king is not on top of board game continues
+            if (isKingOnTurn && pawnKing.XCoordinate == 0)
             {
-                isKingOnTurn = true;
+                return true;
             }
             else
             {
-                isKingOnTurn = false;
-            }
-            #endregion
-
-            if (isKingOnTurn)
-            {
-                if (pawnKing.XCoordinate == 0)
+                // king is trapped by pawns -> game ends, king loses
+                if (!CanKingMove(pawnA, pawnB, pawnC, pawnD, pawnKing) ||
+                    (IsKingTrapped(pawnA, pawnB, pawnC, pawnD, pawnKing) && IsKingTrapped(pawnB, pawnA, pawnC, pawnD, pawnKing) &&
+                    IsKingTrapped(pawnC, pawnA, pawnB, pawnD, pawnKing) && IsKingTrapped(pawnD, pawnA, pawnB, pawnC, pawnKing)))
                 {
-                    isKingWinner = true;
-
-                    //DisplayCurrentEndOnConsole(turn, pawnA, pawnB, pawnC, pawnD, pawnKing);
-
-                    // game ends
                     return true;
                 }
                 else
                 {
-                    //game continues
                     return false;
+                }
+            }
+        }
+
+        internal static bool HasKingWon(int gameTurn, bool gameCondition, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD, Pawn pawnKing)
+        {
+            bool isGameEnded = gameCondition;
+            bool isKingOnTurn = (gameTurn % 2 == 1);
+            // we can only determine king as winner, after game ending
+            if (isGameEnded)
+            {
+                // if king is on top is winner
+                if (isKingOnTurn && pawnKing.XCoordinate == 7)
+                {
+                    return true;
+                }
+                else
+                {
+                    // if king is trapped -> lost
+                    if (!CanKingMove(pawnA, pawnB, pawnC, pawnD, pawnKing))
+                    {
+                        return false;
+                    }//while king is not on top and not trapped
+                    else if (IsKingTrapped(pawnA, pawnB, pawnC, pawnD, pawnKing) && IsKingTrapped(pawnB, pawnA, pawnC, pawnD, pawnKing) &&
+                    IsKingTrapped(pawnC, pawnA, pawnB, pawnD, pawnKing) && IsKingTrapped(pawnD, pawnA, pawnB, pawnC, pawnKing))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
                 }
             }
             else
             {
-                // Kings movements
-                bool canKingGoUpLeft = true;
-                bool canKingGoUpRight = true;
-                bool canKingGoDownLeft = true;
-                bool canKingGoDownRight = true;
-                // checks if king over some border
-                if (pawnKing.XCoordinate == 0)
-                {
-                    canKingGoUpLeft = false;
-                    canKingGoUpRight = false;
-                }
-                else if (pawnKing.XCoordinate == 7)
-                {
-                    canKingGoDownLeft = false;
-                    canKingGoDownRight = false;
-                }
-
-                if (pawnKing.YCoordinate == 0)
-                {
-                    canKingGoUpLeft = false;
-                    canKingGoDownLeft = false;
-                }
-                else if (pawnKing.YCoordinate == 7)
-                {
-                    canKingGoUpRight = false;
-                    canKingGoDownRight = false;
-                }
-
-                // check if king is near pawn
-                canKingGoUpLeft = !(IsAvailableNextPosition(pawnKing.XCoordinate - 1, pawnKing.YCoordinate - 1, pawnA, pawnB, pawnC, pawnD));
-                canKingGoUpRight = !(IsAvailableNextPosition(pawnKing.XCoordinate - 1, pawnKing.YCoordinate + 1, pawnA, pawnB, pawnC, pawnD));
-                canKingGoDownLeft = !(IsAvailableNextPosition(pawnKing.XCoordinate + 1, pawnKing.YCoordinate - 1, pawnA, pawnB, pawnC, pawnD));
-                canKingGoDownRight = !(IsAvailableNextPosition(pawnKing.XCoordinate + 1, pawnKing.YCoordinate + 1, pawnA, pawnB, pawnC, pawnD));
-
-
-                bool isAnyOfKingMovesAvaiable = canKingGoDownRight || canKingGoDownLeft || canKingGoUpLeft || canKingGoUpRight;
-
-                if (!isAnyOfKingMovesAvaiable)
-                {
-                    isKingWinner = false;
-
-                    //DisplayCurrentEndOnConsole(turn, pawnA, pawnB, pawnC, pawnD, pawnKing);
-
-                    // game ends;
-                    return true;
-                }
-
-                if (IsKingTrapped(pawnA, pawnB, pawnC, pawnD, pawnKing) &&
-                    IsKingTrapped(pawnB, pawnA, pawnC, pawnD, pawnKing) &&
-                    IsKingTrapped(pawnC, pawnA, pawnB, pawnD, pawnKing) &&
-                    IsKingTrapped(pawnD, pawnA, pawnB, pawnC, pawnKing))
-                {
-
-                    isKingWinner = true;
-
-                    //DisplayCurrentEndOnConsole(turn, pawnA, pawnB, pawnC, pawnD, pawnKing);
-                    // game ends;
-                    return true;
-                }
-                // game continues
                 return false;
             }
         }
 
+        private static bool CanKingMove(Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD, Pawn pawnKing)
+        {
+            // determine king restrictions
+            bool canKingGoUpLeft = IsKingUpLeftMovementAvailable(pawnKing, pawnA, pawnB, pawnC, pawnD);
+            bool canKingGoDownLeft = IsKingDownLeftMovementAvailable(pawnKing, pawnA, pawnB, pawnC, pawnD);
+            bool canKingGoUpRight = IsKingUpRightMovementAvailable(pawnKing, pawnA, pawnB, pawnC, pawnD);
+            bool canKingGoDownRight = IsKingDownRightMovementAvailable(pawnKing, pawnA, pawnB, pawnC, pawnD);
+            // check if all 
+            bool isAnyOfKingMovesAvaiable = canKingGoDownRight || canKingGoDownLeft || canKingGoUpLeft || canKingGoUpRight;
+            
+            return isAnyOfKingMovesAvaiable;
+        }
+
+        private static bool IsKingUpLeftMovementAvailable(Pawn pawnKing, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD)
+        {
+            bool canKingGoUpLeft = true;
+            // check if king is near border
+            if (pawnKing.XCoordinate == 0 || pawnKing.YCoordinate == 0)
+            {
+                canKingGoUpLeft = false;
+            }
+            // check if pawn is near king
+            canKingGoUpLeft = IsAvailableNextPosition(pawnKing.XCoordinate - 1, pawnKing.YCoordinate - 1, pawnA, pawnB, pawnC, pawnD);
+     
+            return canKingGoUpLeft;
+        }
+
+        private static bool IsKingDownLeftMovementAvailable(Pawn pawnKing, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD)
+        {
+            bool canKingGoDownLeft = true;
+            // check if king is near border
+            if (pawnKing.XCoordinate == 7 || pawnKing.YCoordinate == 0)
+            {
+                canKingGoDownLeft = false;
+            }
+            // check if pawn is near king
+            canKingGoDownLeft = (IsAvailableNextPosition(pawnKing.XCoordinate + 1, pawnKing.YCoordinate - 1, pawnA, pawnB, pawnC, pawnD));
+
+            return canKingGoDownLeft;
+        }
+
+        private static bool IsKingUpRightMovementAvailable(Pawn pawnKing, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD)
+        {
+            bool canKingGoUpRight = true;
+            // check if king is near border
+            if (pawnKing.XCoordinate == 0 || pawnKing.YCoordinate == 7)
+            {
+                canKingGoUpRight = false;
+            }
+            // check if pawn is near king
+            canKingGoUpRight = IsAvailableNextPosition(pawnKing.XCoordinate - 1, pawnKing.YCoordinate + 1, pawnA, pawnB, pawnC, pawnD);
+
+            return canKingGoUpRight;
+        }
+
+        private static bool IsKingDownRightMovementAvailable(Pawn pawnKing, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD)
+        {
+            bool canKingGoDownRight = true;
+            // check if king is near border
+            if (pawnKing.XCoordinate == 7 || pawnKing.YCoordinate == 7)
+            {
+                canKingGoDownRight = false;
+            }
+            // check if pawn is near king
+            canKingGoDownRight = (IsAvailableNextPosition(pawnKing.XCoordinate + 1, pawnKing.YCoordinate + 1, pawnA, pawnB, pawnC, pawnD));
+
+            return canKingGoDownRight;
+        }
+        // TODO : Immediately change name !!!
         private static bool IsKingTrapped(Pawn kingPawn, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD)
         {
             if (kingPawn.XCoordinate == 7)
             {
-                return true;
+                return false;
             }
             else if (kingPawn.YCoordinate > 0 && kingPawn.YCoordinate < 7)
             {
                 if (IsAvailableNextPosition(kingPawn.XCoordinate + 1, kingPawn.YCoordinate + 1, pawnA, pawnB, pawnC, pawnD) &&
                     IsAvailableNextPosition(kingPawn.XCoordinate + 1, kingPawn.YCoordinate - 1, pawnA, pawnB, pawnC, pawnD))
                 {
-                    return true;
+                    return false;
                 }
             }
             else if (kingPawn.YCoordinate == 0)
             {
                 if (IsAvailableNextPosition(kingPawn.XCoordinate + 1, kingPawn.YCoordinate + 1, pawnA, pawnB, pawnC, pawnD))
                 {
-                    return true;
+                    return false;
                 }
             }
-            else if (kingPawn.YCoordinate == 4 + 3)
+            else if (kingPawn.YCoordinate == 7)
             {
                 if (IsAvailableNextPosition(kingPawn.XCoordinate + 1, kingPawn.YCoordinate - 1, pawnA, pawnB, pawnC, pawnD))
                 {
-                    return true;
+                    return false;
                 }
             }
-            return false;
+            return true;
         }
 
         public static bool IsValidMove(int turn, Pawn pawnA, Pawn pawnB, Pawn pawnC, Pawn pawnD, Pawn king)
