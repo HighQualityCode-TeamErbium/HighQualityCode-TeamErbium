8ce912c15e5276e13658edc651b62b7a2d13dbd6
nnaidenov <n.naidenov92@gmail.com>
2013-05-12 15:06:13 +0300

    Add UnitTest files

---
+++ KingSurvival/KingSurvivalTests/ConsoleEngineTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvivalTests
+{
+    [TestClass]
+    public class ConsoleEngineTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ KingSurvival/KingSurvivalTests/GameManagerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KingSurvivalTests
+{
+    [TestClass]
+    public class GameManagerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
