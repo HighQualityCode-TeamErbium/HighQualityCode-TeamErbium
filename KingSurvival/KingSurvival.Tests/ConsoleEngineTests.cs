7d3fcecce030385105830a8151d1bb03173616f3
yoandinkov <yoandinkov@gmail.com>
2013-05-14 10:40:48 +0300

    Added new test, that checks the "bug" in corner

--- KingSurvival/KingSurvival.Tests/ConsoleEngineTests.cs
+++ KingSurvival/KingSurvival.Tests/ConsoleEngineTests.cs
@@ -70,5 +70,35 @@
                 }
             }
         }
+
+        [TestMethod]
+        public void TestKingIsTrappedInRightDownCorner()
+        {
+            string inputCommands = string.Format(
+                "kur{0}bdl{0}kur{0}bdr{0}kul{0}bdr{0}kdr{0}bdr{0}kdr{0}bdr{0}kdr{0}bdr",
+                Environment.NewLine);
+
+            using (StringWriter stringWriter = new StringWriter())
+            {
+                Console.SetOut(stringWriter);
+
+                using (StringReader stringReader = new StringReader(inputCommands))
+                {
+                    Console.SetIn(stringReader);
+
+                    IEngine engine = new ConsoleEngine();
+                    engine.Run();
+
+                    char[] separators = Environment.NewLine.ToCharArray();
+                    string consoleOutput = stringWriter.ToString();
+                    string[] outputLines = consoleOutput.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+                    string expected = "King loses in 6 turns...";
+                    int lastLineIndex = outputLines.Length - 1;
+                    string actual = outputLines[lastLineIndex];
+                    Assert.AreEqual<string>(expected, actual);
+                }
+            }
+        }
     }
 }
